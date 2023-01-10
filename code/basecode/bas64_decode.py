31d1dda727c124c2b182b62b2be4f5c0b50332a2
k-dalgaard <y009367@sit-mgtnet002.prod.sitad.dk>
2023-01-10 21:23:37 +0100

    updated

--- code/basecode/bas64_decode.py
+++ code/basecode/bas64_decode.py
@@ -6,6 +6,5 @@ def decode_base64(token):
     return decoded_token
 
 # test the function
-token = "T0JvRk8yS21VZmtyQzhsZEx1SUE="
 token = input("enter base64 token to decode: ")
 print(decode_base64(token))
